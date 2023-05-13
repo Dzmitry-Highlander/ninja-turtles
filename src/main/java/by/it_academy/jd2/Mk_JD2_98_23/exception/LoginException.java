d98402999a6700789d64154244617974f1b534ad
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-05-13 22:56:39 +0300

    feat: LoginException, UserCreateException

---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/exception/LoginException.java
@@ -0,0 +1,7 @@
+package by.it_academy.jd2.Mk_JD2_98_23.exception;
+
+public class LoginException extends Exception {
+    public LoginException() {
+        super("Invalid username or password!");
+    }
+}
---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/exception/UserCreateException.java
@@ -0,0 +1,7 @@
+package by.it_academy.jd2.Mk_JD2_98_23.exception;
+
+public class UserCreateException extends Exception {
+    public UserCreateException() {
+        super("All fields must be filled!");
+    }
+}
