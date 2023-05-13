b87d674b4d6ede411f3e0b360bd1291899376bde
Pavel <KoPash.92@gmail.com>
2023-05-13 22:41:42 +0300

    Third memory renew

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/dao/memory/UserRoleMemoryDao.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/dao/memory/UserRoleMemoryDao.java
@@ -1,4 +1,4 @@
-package by.it_academy.jd2.Mk_JD2_98_23.dao.memory.factory;
+package by.it_academy.jd2.Mk_JD2_98_23.dao.memory;
 
 import by.it_academy.jd2.Mk_JD2_98_23.core.dto.UserRoleDTO;
 import by.it_academy.jd2.Mk_JD2_98_23.dao.api.IUserRoleDao;
