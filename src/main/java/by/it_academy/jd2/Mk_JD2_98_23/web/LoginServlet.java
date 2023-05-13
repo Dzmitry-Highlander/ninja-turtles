b3cdd677d6503978d6af48fb17b8f9dc8767d8ec
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-05-13 23:54:08 +0300

    refactor LoginServlet

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/web/LoginServlet.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/web/LoginServlet.java
@@ -9,16 +9,13 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-
 import java.io.IOException;
 
 @WebServlet("/api/login")
 public class LoginServlet extends HttpServlet {
-
     private static final String USERNAME_PARAM_NAME = "username";
     private static final String PASSWORD_PARAM_NAME = "password";
     private static final String USER_SESSION_ATTRIBUTE_NAME = "user";
-
     private final IUserService userService;
 
     public LoginServlet() {
