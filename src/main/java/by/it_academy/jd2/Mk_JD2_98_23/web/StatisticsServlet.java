2ab1e76c5221b592361d855a554a922acd29b83d
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-05-13 23:59:21 +0300

    add StatisticsServlet *(empty)

---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/web/StatisticsServlet.java
@@ -0,0 +1,9 @@
+package by.it_academy.jd2.Mk_JD2_98_23.web;
+
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/api/admin/statistics")
+public class StatisticsServlet extends HttpServlet {
+}
