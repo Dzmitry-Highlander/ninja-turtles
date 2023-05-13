464390d160484e7dc19ce7b37aa33d4d8f9170e5
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-05-14 01:13:19 +0300

    refactor: LoginServlet

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/web/LoginServlet.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/web/LoginServlet.java
@@ -24,28 +24,20 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // Getting data from a request
         String username = req.getParameter(USERNAME_PARAM_NAME);
         String password = req.getParameter(PASSWORD_PARAM_NAME);
+        UserDTO user = userService.validate(username, password);
+        HttpSession session = req.getSession(true);
 
-        // Checking for empty fields
         if (username == null || username.isEmpty() || password == null || password.isEmpty()) {
             resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Username or password is empty");
             return;
         }
-
-        // Checking the validity of the username and password
-        UserDTO user = userService.validate(username, password);
         if (user == null) {
             resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid username or password");
             return;
         }
-
-        // Adding a user to a session
-        HttpSession session = req.getSession(true);
         session.setAttribute(USER_SESSION_ATTRIBUTE_NAME, user);
-
-        // Sending a response to the client
         resp.setStatus(HttpServletResponse.SC_OK);
         resp.getWriter().write("Authorization was successful!");
     }
