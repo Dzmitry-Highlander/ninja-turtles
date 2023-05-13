e4d68d90fd49a44e5d94095c7844e6e9721eae54
NickSuzdalev <Nick.Suzdalev@gmail.com>
2023-05-13 23:13:04 +0300

    add: MessageService

---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/MessageService.java
@@ -0,0 +1,75 @@
+package by.it_academy.jd2.Mk_JD2_98_23.service;
+
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.MessageCreateDTO;
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.MessageDTO;
+import by.it_academy.jd2.Mk_JD2_98_23.core.dto.UserDTO;
+import by.it_academy.jd2.Mk_JD2_98_23.dao.api.IMessageDao;
+import by.it_academy.jd2.Mk_JD2_98_23.service.api.IMessageService;
+import by.it_academy.jd2.Mk_JD2_98_23.service.api.IUserService;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.OptionalInt;
+
+public class MessageService implements IMessageService {
+
+    private  IMessageDao messageDao;
+    private IUserService userService;
+
+
+    public MessageService(IMessageDao messageDao) {
+        this.messageDao = messageDao;
+    }
+
+    @Override
+    public void setService(IUserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public List<MessageDTO> get() {
+        return messageDao.get();
+    }
+
+    @Override
+    public MessageDTO get(int id) {
+        return messageDao.get(id);
+    }
+
+    @Override
+    public MessageDTO save(MessageCreateDTO item) {
+        OptionalInt maxId = this.get()
+                .stream()
+                .mapToInt(MessageDTO::getId)
+                .max();
+        int maxCurrentId = maxId.isPresent() ? maxId.getAsInt() : 0;
+
+        MessageDTO dto = new MessageDTO();
+        dto.setId(maxCurrentId + 1);
+        dto.setDateTime(LocalDateTime.now());
+        dto.setFrom(item.getFrom());
+        dto.setTo(item.getTo());
+        dto.setText(item.getText());
+
+        return messageDao.save(dto);
+    }
+
+    @Override
+    public List<MessageDTO> getMessagesForUser(int userId) {
+        List<MessageDTO> messages = new ArrayList<>();
+        // Get user object by id
+        UserDTO user = userService.get(userId);
+        if (user == null) {
+            return messages;
+        }
+        // Get a list of all messages from DAO
+        List<MessageDTO> allMessages = messageDao.get();
+        for (MessageDTO message : allMessages) {
+            if (message.getFrom().equals(user) || message.getTo().equals(user)) {
+                messages.add(message);
+            }
+        }
+        return messages;
+    }
+}
