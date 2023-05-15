c082e11544bcfe5a3f287ed84bcb35bedf2d6e05
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-05-15 05:42:03 +0300

    refactor  StatisticsService (no doGet)

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/web/StatisticsServlet.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/web/StatisticsServlet.java
@@ -1,9 +1,25 @@
 package by.it_academy.jd2.Mk_JD2_98_23.controllers.web;
 
 
+import by.it_academy.jd2.Mk_JD2_98_23.service.api.IStatisticsService;
+import by.it_academy.jd2.Mk_JD2_98_23.service.factory.StatisticsServiceFactory;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/api/admin/statistics")
 public class StatisticsServlet extends HttpServlet {
+    private final IStatisticsService statisticsService;
+
+    public StatisticsServlet() {
+        this.statisticsService = StatisticsServiceFactory.getInstance();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
 }
