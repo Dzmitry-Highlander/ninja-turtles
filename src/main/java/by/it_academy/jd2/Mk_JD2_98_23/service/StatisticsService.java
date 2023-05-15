fa734b939155c98b74e8e37725d626d1e3102be0
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-05-15 05:23:26 +0300

    refactor  StatisticsService

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/StatisticsService.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/StatisticsService.java
@@ -1,18 +1,18 @@
 package by.it_academy.jd2.Mk_JD2_98_23.service;
 
-import by.it_academy.jd2.Mk_JD2_98_23.controllers.listeners.ActiveSessionListener;
+import by.it_academy.jd2.Mk_JD2_98_23.controllers.listeners.ActiveSessionsListener;
 import by.it_academy.jd2.Mk_JD2_98_23.service.api.IMessageService;
 import by.it_academy.jd2.Mk_JD2_98_23.service.api.IStatisticsService;
 import by.it_academy.jd2.Mk_JD2_98_23.service.api.IUserService;
 
 
 public class StatisticsService implements IStatisticsService {
-    private final ActiveSessionListener sessionListener;
+    private final ActiveSessionsListener sessionListener;
     private final IMessageService messageService;
     private final IUserService userService;
 
 
-    public StatisticsService(ActiveSessionListener sessionListener, IMessageService messageService, IUserService userService) {
+    public StatisticsService(ActiveSessionsListener sessionListener, IMessageService messageService, IUserService userService) {
         this.sessionListener = sessionListener;
         this.messageService = messageService;
         this.userService = userService;
