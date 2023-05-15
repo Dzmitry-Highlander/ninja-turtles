7eb2b2ee796816db03ad94755af9e80bc66a3375
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-05-15 05:22:36 +0300

    refactor  ActiveSessionsListener

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/listeners/ActiveSessionsListener.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/listeners/ActiveSessionsListener.java
@@ -3,7 +3,7 @@ package by.it_academy.jd2.Mk_JD2_98_23.controllers.listeners;
 import jakarta.servlet.http.HttpSessionEvent;
 import jakarta.servlet.http.HttpSessionListener;
 
-public class ActiveSessionListener implements HttpSessionListener {
+public class ActiveSessionsListener implements HttpSessionListener {
     private static int activeSessions = 0;
 
     @Override
