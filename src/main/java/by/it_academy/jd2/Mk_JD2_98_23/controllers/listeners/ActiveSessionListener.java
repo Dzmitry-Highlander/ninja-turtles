8d80128c411a4076a3e209c371f296ce3292c80b
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-05-15 04:36:15 +0300

    refactor ActiveSessionListener

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/listeners/ActiveSessionListener.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/listeners/ActiveSessionListener.java
@@ -1,4 +1,4 @@
-package by.it_academy.jd2.Mk_JD2_98_23.controllers.listiners;
+package by.it_academy.jd2.Mk_JD2_98_23.controllers.listeners;
 
 import jakarta.servlet.http.HttpSessionEvent;
 import jakarta.servlet.http.HttpSessionListener;
