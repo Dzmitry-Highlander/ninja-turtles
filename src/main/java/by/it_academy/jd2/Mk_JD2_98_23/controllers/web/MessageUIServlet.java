fa9b82c89cf760275f6f161039215636361ccd99
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-05-16 11:16:43 +0300

    add MessageUIServlet

---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/web/MessageUIServlet.java
@@ -0,0 +1,26 @@
+package by.it_academy.jd2.Mk_JD2_98_23.controllers.web;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/ui/user/message")
+public class MessageUIServlet extends HttpServlet {
+    private static final String USER_SESSION_ATTRIBUTE_NAME = "user";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if (session == null || session.getAttribute(USER_SESSION_ATTRIBUTE_NAME) == null) {
+            resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "User not authorized");
+            return;
+        }
+
+        req.getRequestDispatcher("/api/message").forward(req, resp);
+    }
+}
