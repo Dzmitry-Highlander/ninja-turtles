c683bfa9bbd954d63a6fc46797959f2d09f08091
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-05-18 13:00:26 +0300

    refactor UserUIServlet.java +check

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/web/UserUIServlet.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/web/UserUIServlet.java
@@ -5,12 +5,21 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/ui/signUp")
 public class UserUIServlet extends HttpServlet {
+    private static final String USER_SESSION_ATTRIBUTE_NAME = "user";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/ui/signUp.jsp").forward(req, resp);
+        HttpSession session = req.getSession();
+
+        if ((session != null) && (session.getAttribute(USER_SESSION_ATTRIBUTE_NAME) != null)) {
+            req.getRequestDispatcher("/ui/").forward(req, resp);
+        } else {
+            req.getRequestDispatcher("/ui/signUp.jsp").forward(req, resp);
+        }
     }
 }
