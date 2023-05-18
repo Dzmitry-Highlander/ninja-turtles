01ff3ff80c8d765937bd6c410b0d8c2caaf262de
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-05-18 03:58:47 +0300

    refactor InMessagesUIServlet.java

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/web/InMessagesUIServlet.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/controllers/web/InMessagesUIServlet.java
@@ -12,6 +12,6 @@ import java.io.IOException;
 public class InMessagesUIServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/ui/user/InMessages.jsp").forward(req, resp);
+        req.getRequestDispatcher("/ui/user/inMessages.jsp").forward(req, resp);
     }
 }
