98775c997e16f158263a0fdf335c3f042cebd059
Prakharchuk Viktar <va.p.gramm1ng@gmail.com>
2023-05-18 03:59:38 +0300

    refactor IMessageService.java + method  getMessagesUser

--- src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/api/IMessageService.java
+++ src/main/java/by/it_academy/jd2/Mk_JD2_98_23/service/api/IMessageService.java
@@ -2,12 +2,12 @@ package by.it_academy.jd2.Mk_JD2_98_23.service.api;
 
 import by.it_academy.jd2.Mk_JD2_98_23.core.dto.MessageCreateDTO;
 import by.it_academy.jd2.Mk_JD2_98_23.core.dto.MessageDTO;
-
-import java.util.ArrayList;
 import java.util.List;
 
-public interface IMessageService extends ICRUDService <MessageDTO, MessageCreateDTO>{
+public interface IMessageService extends ICRUDService<MessageDTO, MessageCreateDTO> {
     List<MessageDTO> getMessagesForUser(int userId);
+
+    List<MessageDTO> getMessagesUser(int userId);
 
     void setService(IUserService userService);
 
